71106dc4b69df09331b8a1110f543bd789540479
Daniel Campos <giusniyyel@gmail.com>
2023-03-26 08:32:07 -0600

    :tada: initial commit

---
+++ src/main/java/org/giusniyyel/webshopapi/WebshopApiApplication.java
@@ -0,0 +1,13 @@
+package org.giusniyyel.webshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/giusniyyel/webshopapi/WebshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.giusniyyel.webshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
